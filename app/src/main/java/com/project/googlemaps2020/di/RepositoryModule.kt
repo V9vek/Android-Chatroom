2e3918dc4f829061d15d76bd18fe074ea7bad841
5abhisheksaxena <5abhisheksaxena@gmail.com>
2020-10-11 15:18:01 +0530

    Update DI scopes

--- app/src/main/java/com/project/googlemaps2020/di/RepositoryModule.kt
+++ app/src/main/java/com/project/googlemaps2020/di/RepositoryModule.kt
@@ -1,5 +1,7 @@
 package com.project.googlemaps2020.di
 
+import android.content.Context
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.StorageReference
@@ -7,18 +9,27 @@ import com.project.googlemaps2020.repository.MainRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
 import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(ActivityRetainedComponent::class)
 object RepositoryModule {
 
-    @Singleton
+    @ActivityRetainedScoped
     @Provides
     fun provideMainRepository(
         auth: FirebaseAuth,
         storageRef: StorageReference,
         firestoreRef: FirebaseFirestore
     ) = MainRepository(auth, storageRef, firestoreRef)
+
+    @ActivityRetainedScoped
+    @Provides
+    fun provideFusedLocationProviderClient(
+        @ApplicationContext app: Context
+    ) = FusedLocationProviderClient(app)
 }
